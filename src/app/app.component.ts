acd916ce87fc33153f7989eeed86ecae1857c5dc
Mukesh Kumar Rai <mukesh.fiv@gmail.com>
2020-03-01 15:56:52 +0530

    track by code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MYANG7APP';
+  students:any[];
+  constructor(){
+   this.students= [
+     {
+       studentid:1,
+       name:'Mukesh',
+       age:23
+
+     },
+     {
+      studentid:2,
+      name:'Mukesh-2',
+      age:23
+
+    },
+    {
+      studentid:3,
+      name:'Mukesh-3',
+      age:23
+
+    }, {
+      studentid:4,
+      name:'Mukesh-4',
+      age:23
+
+    },
+    {
+      studentid:5,
+      name:'Mukesh-5',
+      age:23
+
+    }
+   ]
+  }
+  getmoreStudent():void{
+    this.students= [
+      {
+        studentid:1,
+        name:'Mukesh',
+        age:23
+ 
+      },
+      {
+       studentid:2,
+       name:'Mukesh-2',
+       age:23
+ 
+     },
+     {
+       studentid:3,
+       name:'Mukesh-3',
+       age:23
+ 
+     }, {
+       studentid:4,
+       name:'Mukesh-4',
+       age:23
+ 
+     },
+     {
+       studentid:5,
+       name:'Mukesh-5',
+       age:23
+ 
+     },
+     {
+      studentid:6,
+      name:'Anvi',
+      age:23
+
+    }
+    ]
+  }
 }
